c0f40439c163c6c0ae2be390ac55114b34caec47
Leticia Speda <leticia@Leticias-MacBook-Air.local>
2024-01-06 12:56:10 -0300

    feat: configuracoes iniciais

---
+++ ArquiteturaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArquiteturaApp
+//
+//  Created by Leticia Speda on 06/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
