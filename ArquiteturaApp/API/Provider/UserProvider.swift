5fbb8d5e6f34ff81002c77e577437a66680c023b
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-06 20:44:07 -0300

    feat: criado classe e protocolo do provider

--- ArquiteturaApp/API/Provider/UserProvider.swift
+++ ArquiteturaApp/API/Provider/UserProvider.swift
@@ -1,8 +1,24 @@
-//
-//  UserProvider.swift
-//  ArquiteturaApp
-//
-//  Created by Leticia Speda on 06/01/24.
-//
-
 import Foundation
+import FirebaseAuth
+
+protocol UserProviderProtocol {
+    func register(parameters: [AnyHashable: Any], completionHadler: @escaping(Result<UserModel, Error>) -> Void)
+    
+    func login(parameters: [AnyHashable: Any],  completionHadler: @escaping(Result<UserModel, Error>) -> Void)
+}
+
+final class UserProvider: UserProviderProtocol {
+    private lazy var auth = Auth.auth()
+    
+    func login(parameters: [AnyHashable : Any], completionHadler: @escaping (Result<UserModel, Error>) -> Void) {
+        let body: NSDictionary = parameters["body"] as! NSDictionary
+        let userModel = body["userModel"] as! UserModel
+        
+        self.auth.signIn(withEmail: userModel.email, password: userModel.password)
+    }
+    
+    func register(parameters: [AnyHashable : Any], completionHadler: @escaping (Result<UserModel, Error>) -> Void) {
+        
+    }
+    
+}
