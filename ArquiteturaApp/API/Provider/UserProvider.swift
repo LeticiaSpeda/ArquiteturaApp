3e2c3c2954d39b13b4fd6ccddbc8235000d923fa
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-07 16:45:58 -0300

    refactor: retirado o texto fixo e colocado em uma struct

--- ArquiteturaApp/API/Provider/UserProvider.swift
+++ ArquiteturaApp/API/Provider/UserProvider.swift
@@ -11,10 +11,16 @@ final class UserProvider: UserProviderProtocol {
     private lazy var auth = Auth.auth()
     
     func login(parameters: [AnyHashable : Any], completionHadler: @escaping (Result<UserModel, Error>) -> Void) {
-        let body: NSDictionary = parameters["body"] as! NSDictionary
-        let userModel = body["userModel"] as! UserModel
+        let body: NSDictionary = parameters[Constants.ParametersKeys.body] as! NSDictionary
+        let userModel = body[Constants.ParametersKeys.userModel] as! UserModel
         
-        self.auth.signIn(withEmail: userModel.email, password: userModel.password)
+        self.auth.signIn(withEmail: userModel.email, password: userModel.password) { (result, error) in
+            if let error = error {
+                completionHadler(.failure(error))
+            } else {
+                completionHadler(.success(userModel))
+            }
+        }
     }
     
     func register(parameters: [AnyHashable : Any], completionHadler: @escaping (Result<UserModel, Error>) -> Void) {
