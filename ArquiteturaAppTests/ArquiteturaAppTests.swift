65c4ccd59b617f7f5253f5340efccf6c25bca717
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-08 12:07:54 -0300

    test: criado arquivo para testa a API

--- ArquiteturaAppTests/ArquiteturaAppTests.swift
+++ ArquiteturaAppTests/ArquiteturaAppTests.swift
@@ -1,35 +1,51 @@
-//
-//  ArquiteturaAppTests.swift
-//  ArquiteturaAppTests
-//
-//  Created by Leticia Speda on 08/01/24.
-//
-
 import XCTest
+@testable import ArquiteturaApp
 
 final class ArquiteturaAppTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func test_Register() {
+        let manager = UserManager(business: UserBusiness())
+        
+        manager.register(email: "aaaa", password: "a123456") { userModel in
+            print(userModel)
+        } failureHandler: { error in
+            print(error)
         }
-    }
+        
+        manager.register(email: "test@mail.com", password: "aa") { userModel in
+            print(userModel)
+        } failureHandler: { error in
+            print(error)
+        }
+        
+        manager.register(email: "test@mail.com", password: "123456") { userModel in
+            print(userModel)
+        } failureHandler: { error in
+            print(error)
+        }
 
+    }
+    
+    func test_Login() {
+        let manager = UserManager(business: UserBusiness())
+        
+        manager.login(email: "aaaa", password: "a123456") { userModel in
+            print(userModel)
+        } failureHandler: { error in
+            print(error)
+        }
+        
+        manager.login(email: "test@mail.com", password: "aa") { userModel in
+            print(userModel)
+        } failureHandler: { error in
+            print(error)
+        }
+        
+        manager.login(email: "test@mail.com", password: "123456") { userModel in
+            print(userModel)
+        } failureHandler: { error in
+            print(error)
+        }
+
+    }
 }
