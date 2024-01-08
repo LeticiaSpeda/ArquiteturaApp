638293088920432e016f6ee4b9fdef686b5a422a
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-08 11:02:01 -0300

    refactor: ajustando funcoes do paramentro da funcao no protocolo do Manager

--- ArquiteturaApp/API/Manager/UserManager.swift
+++ ArquiteturaApp/API/Manager/UserManager.swift
@@ -1,23 +1,17 @@
 import Foundation
 
 protocol UserManagerProtocol  {
-    func login(email: String, password: String, successHandler: @escaping(UserModel) -> Void, failureHandler: @escaping(Error) -> Void)
-    func register(email: String, password: String, completionHandler: @escaping(Result<UserModel, Error>) -> Void)
+    func login(email: String, password: String, completionHandler: @escaping(Result<UserModel, Error>) -> Void)
+    
+    func register(email: String, password: String, successHandler: @escaping(UserModel) -> Void, failureHandler: @escaping(Error) -> Void)
 }
 
 final class UserManager: UserManagerProtocol {
-    private let business: UserBusinessProtocol
-    
-    init(business: UserBusinessProtocol) {
-        self.business = business
-    }
-    
-    func login(email: String, password: String, successHandler: @escaping (UserModel) -> Void, failureHandler: @escaping (Error) -> Void) {
-    }
-    
-    func register(email: String, password: String, completionHandler: @escaping (Result<UserModel, Error>) -> Void) {
+    func login(email: String, password: String, completionHandler: @escaping (Result<UserModel, Error>) -> Void) {
         <#code#>
     }
     
-    
+    func register(email: String, password: String, successHandler: @escaping (UserModel) -> Void, failureHandler: @escaping (Error) -> Void) {
+        <#code#>
+    }
 }
