b4416b78e15b838d01e1eeda32c2bfce0420a02e
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-15 09:10:40 -0300

    feat: criado registerPresenter

--- ArquiteturaApp/Register/RegisterPresent.swift
+++ ArquiteturaApp/Register/RegisterPresent.swift
@@ -1,8 +1,21 @@
-//
-//  RegisterPresent.swift
-//  ArquiteturaApp
-//
-//  Created by Leticia Speda on 15/01/24.
-//
-
 import Foundation
+
+protocol RegisterPresentDelegate {
+    func showMessage(title: String, message: String)
+    func goHome()
+}
+
+final class RegisterPresent {
+    var delegate: RegisterPresentDelegate?
+    
+    func register(userModel: UserModel) {
+        let manager = UserManager(business: UserBusiness())
+        
+        manager.register(email: userModel.email, password: userModel.password) { [weak self] model in
+            self?.delegate?.goHome()
+        } failureHandler: { [weak self] error in
+            self?.delegate?.showMessage(title: "Erro", message: "\(error.localizedDescription)")
+        }
+
+    }
+}
